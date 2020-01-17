13e2dcd93aac2ac7e1e95e37aa44e93a5750a29a
AriAlavi <arianalavi1@gmail.com>
2020-01-16 21:58:06 -0800

    very basics

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 import main.views as main_views
 urlpatterns = [
-    path('', main_views.home, name='home')
+    path('', main_views.home, name='home'),
+    path('register/', main_views.register, name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, "main/home.html")
+
+def register(request):
+    # if request.user.profile:
+    #     return redirect('home')
+    form = UserCreationForm(request.POST or None)
+    context= {
+        'form' : form
+    }
+    return render(request, "main/register.html", context)
