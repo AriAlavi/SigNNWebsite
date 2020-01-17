ed7c9e54b00f645c0789ba7a576867a9defa659c
AriAlavi <arianalavi1@gmail.com>
2020-01-16 22:45:30 -0800

    register

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
+from main.forms import NewProfileForm
+
 def home(request):
     return render(request, "main/home.html")
 
@@ -8,8 +10,17 @@ def register(request):
     # if request.user.profile:
     #     return redirect('home')
     form = UserCreationForm(request.POST or None)
+    profile_form = NewProfileForm(request.POST or None)
+    if form.is_valid() and profile_form.is_valid():
+        user = form.save()
+        profile = profile_form.save(commit=False)
+        profile.user = user
+        user.email = profile.email
+        user.save()
+        profile.save()
+        return redirect('home')
     context= {
         'form' : form,
-        'register' : "e"
+        'register' : profile_form,
     }
     return render(request, "main/register.html", context)
