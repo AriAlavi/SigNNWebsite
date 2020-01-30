e7dd97662f8577fb2b92f1e09981a00e86d937fb
gilgahex <orrbifold@gmail.com>
2020-01-29 16:46:12 -0800

    Use this library

--- main/static/main/camera.js
+++ main/static/main/camera.js
@@ -1,11 +1,20 @@
 var video = document.querySelector("#videoElement");
 
+var play = document.getElementById("play");
+var pause = document.getElementById("pause");
+var stop = document.getElementById("stop");
+
 if (navigator.mediaDevices.getUserMedia) {
+	
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
+		
       video.srcObject = stream;
+      
     })
+    
     .catch(function (err0r) {
       console.log("Something went wrong!");
     });
+    
 }
