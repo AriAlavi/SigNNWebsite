e3a2982bcc99c5914627f8646d69220448d0b9ba
AriAlavi <arianalavi1@gmail.com>
2020-01-29 16:07:11 -0800

    Added auto-reload system

---
+++ reload.sh
@@ -0,0 +1,6 @@
+source env/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+sudo service apache2 restart
